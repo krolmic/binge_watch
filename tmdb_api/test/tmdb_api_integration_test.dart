9d222dc20a2d1388d8d0d2a7e34ed194be6dc81e
Michael Krol <michael.krol@ambient-innovation.com>
2020-01-05 23:17:10 +0100

    Extend TMDB API integration tests

--- tmdb_api/test/tmdb_api_integration_test.dart
+++ tmdb_api/test/tmdb_api_integration_test.dart
@@ -18,7 +18,23 @@ void main() {
       expect(result, isMap);
       expect(result['results'], isList);
       expect(result['results'], isNotEmpty);
-      expect(result['results'][0].containsKey('title'), isTrue);
+
+      final Map<String, dynamic> movieToTest = result['results'][0];
+      expect(movieToTest['title'], isA<String>());
+      expect(movieToTest['popularity'], isNotNaN);
+      expect(movieToTest['vote_count'], isA<int>());
+      expect(movieToTest['video'], isA<bool>());
+      expect(movieToTest['adult'], isA<bool>());
+      expect(movieToTest['original_title'], isA<String>());
+      expect(movieToTest['poster_path'], isA<String>());
+      expect(movieToTest['release_date'], isA<String>());
+      expect(DateTime.parse(movieToTest['release_date']), isNotNull);
+      expect(movieToTest['id'], isA<int>());
+      expect(movieToTest['backdrop_path'], isA<String>());
+      expect(movieToTest['original_language'], isA<String>());
+      expect(movieToTest['genre_ids'], isList);
+      expect(movieToTest['vote_average'], isNotNaN);
+      expect(movieToTest['overview'], isA<String>());
     });
 
     test('getDiscoverMoviesResults genres test', () async {
@@ -50,6 +66,23 @@ void main() {
       expect(result, isMap);
       expect(result['results'], isList);
       expect(result['results'], isNotEmpty);
+
+      final Map<String, dynamic> tvShowToTest = result['results'][0];
+      expect(tvShowToTest['original_name'], isA<String>());
+      expect(tvShowToTest['genre_ids'], isList);
+      expect(tvShowToTest['name'], isA<String>());
+      expect(tvShowToTest['popularity'], isNotNaN);
+      expect(tvShowToTest['original_name'], isA<String>());
+      expect(tvShowToTest['origin_country'], isList);
+      expect(tvShowToTest['vote_count'], isA<int>());
+      expect(tvShowToTest['first_air_date'], isA<String>());
+      expect(DateTime.parse(tvShowToTest['first_air_date']), isNotNull);
+      expect(tvShowToTest['backdrop_path'], isA<String>());
+      expect(tvShowToTest['original_language'], isA<String>());
+      expect(tvShowToTest['id'], isA<int>());
+      expect(tvShowToTest['vote_average'], isNotNaN);
+      expect(tvShowToTest['overview'], isA<String>());
+      expect(tvShowToTest['poster_path'], isA<String>());
     });
 
     test('getDiscoverTVShowsResults genres page test', () async {
@@ -74,6 +107,29 @@ void main() {
           await api.getMovieDetails(movieTestId);
       expect(result, isMap);
       expect(result['id'], equals(movieTestId));
+
+      // Verify data types
+      expect(result['title'], isA<String>());
+      expect(result['popularity'], isNotNaN);
+      expect(result['vote_count'], isA<int>());
+      expect(result['video'], isA<bool>());
+      expect(result['adult'], isA<bool>());
+      expect(result['original_title'], isA<String>());
+      expect(result['poster_path'], isA<String>());
+      expect(result['release_date'], isA<String>());
+      expect(DateTime.parse(result['release_date']), isNotNull);
+      expect(result['id'], isA<int>());
+      expect(result['backdrop_path'], isA<String>());
+      expect(result['original_language'], isA<String>());
+      expect(result['genres'], isList);
+      expect(result['vote_average'], isNotNaN);
+      expect(result['overview'], isA<String>());
+      expect(result['budget'], isNotNaN);
+      expect(result['homepage'], isA<String>());
+      expect(result['tagline'], isA<String>());
+      expect(result['production_companies'], isList);
+      expect(result['production_countries'], isList);
+      expect(result['spoken_languages'], isList);
     });
 
     test('getTVShowDetails test', () async {
@@ -90,6 +146,36 @@ void main() {
           await api.getTVShowDetails(tvShowTestId);
       expect(result, isMap);
       expect(result['id'], equals(tvShowTestId));
+
+      // Verify data types
+      expect(result['original_name'], isA<String>());
+      expect(result['genres'], isList);
+      expect(result['name'], isA<String>());
+      expect(result['popularity'], isNotNaN);
+      expect(result['original_name'], isA<String>());
+      expect(result['origin_country'], isList);
+      expect(result['vote_count'], isA<int>());
+      expect(result['first_air_date'], isA<String>());
+      expect(DateTime.parse(result['first_air_date']), isNotNull);
+      expect(result['last_air_date'], isA<String>());
+      expect(DateTime.parse(result['last_air_date']), isNotNull);
+      expect(result['backdrop_path'], isA<String>());
+      expect(result['original_language'], isA<String>());
+      expect(result['id'], isA<int>());
+      expect(result['vote_average'], isNotNaN);
+      expect(result['overview'], isA<String>());
+      expect(result['poster_path'], isA<String>());
+      expect(result['production_companies'], isList);
+      expect(result['status'], isA<String>());
+      expect(result['created_by'], isList);
+      expect(result['in_production'], isA<bool>());
+      expect(result['languages'], isList);
+      expect(result['last_episode_to_air'], isMap);
+      expect(result['networks'], isList);
+      expect(result['number_of_episodes'], isA<int>());
+      expect(result['number_of_seasons'], isA<int>());
+      expect(result['type'], isA<String>());
+      expect(result['seasons'], isList);
     });
 
     test('getListMoviesGenreIDsResults test', () async {
