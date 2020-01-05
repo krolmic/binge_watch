f57a41ca2ccfbaead48bf5498a94622d28bc8b19
Michael Krol <michael.krol@ambient-innovation.com>
2020-01-05 23:16:31 +0100

    Extend Movie model by further parameters

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -11,10 +11,19 @@ class Movie extends MovieTVShowBase {
     List<int> genreIds,
     double voteAverage,
     String overview,
+    String homepage,
+    List<Map<String, dynamic>> productionCompanies,
+    String status,
     this.adult,
     this.originalTitle,
     this.title,
     this.releaseDate,
+    this.belongsToCollection,
+    this.budget,
+    this.imdbId,
+    this.productionCountries,
+    this.spokenLanguages,
+    this.tagLine,
   }) : super(
           popularity: popularity,
           voteCount: voteCount,
@@ -25,23 +34,44 @@ class Movie extends MovieTVShowBase {
           genreIds: genreIds,
           voteAverage: voteAverage,
           overview: overview,
+          homepage: homepage,
+          productionCompanies: productionCompanies,
+          status: status,
         );
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      popularity: json['popularity'],
+      popularity:
+          json.containsKey('popularity') ? json['popularity'].toDouble() : null,
       voteCount: json['vote_count'],
       posterPath: json['poster_path'],
       id: json['id'],
       backdropPath: json['backdrop_path'],
       originalLanguage: json['original_language'],
-      genreIds: json['genre_ids'],
-      voteAverage: json['vote_average'],
+      genreIds: json.containsKey('genre_ids')
+          ? json['genre_ids']
+          : json.containsKey('genres')
+              ? List<int>.from(json['genres'].map((dynamic e) => e['id']))
+              : null,
+      voteAverage: json.containsKey('vote_average')
+          ? json['vote_average'].toDouble()
+          : null,
       overview: json['overview'],
       adult: json['adult'],
       originalTitle: json['original_title'],
       title: json['title'],
-      releaseDate: DateTime.parse(json['release_date']),
+      releaseDate: json.containsKey('release_date')
+          ? DateTime.parse(json['release_date'])
+          : null,
+      belongsToCollection: json['belongs_to_collection'],
+      budget: json.containsKey('budget') ? json['budget'].toDouble() : null,
+      homepage: json['homepage'],
+      imdbId: json['imdb_id'],
+      productionCompanies: json['production_companies'],
+      productionCountries: json['production_countries'],
+      spokenLanguages: json['spoken_languages'],
+      status: json['status'],
+      tagLine: json['tagline'],
     );
   }
 
@@ -49,4 +79,10 @@ class Movie extends MovieTVShowBase {
   final String originalTitle;
   final String title;
   final DateTime releaseDate;
+  final Map<String, dynamic> belongsToCollection;
+  final double budget;
+  final String imdbId;
+  final List<Map<String, dynamic>> productionCountries;
+  final List<Map<String, dynamic>> spokenLanguages;
+  final String tagLine;
 }
